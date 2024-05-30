2434bc22752027daaf762705ccb242708eaee3ba
hyewon218 <won_11111@naver.com>
2024-05-30 21:45:57 +0900

    시장 모듈 하위 경로 설정 및 <Outlet> 추가

--- src/router/root.js
+++ src/router/root.js
@@ -5,6 +5,8 @@ import {createBrowserRouter} from "react-router-dom" ;
 const Loading = <div>Loading....</div>
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
+const MarketIndex = lazy(() => import("../pages/markets/IndexPage"))
+const MarketList = lazy(() => import("../pages/markets/ListPage"))
 
 const root = createBrowserRouter([
     {
@@ -14,6 +16,16 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}> <About/></Suspense>
+    },
+    {
+        path: "markets",
+        element: <Suspense fallback={Loading}><MarketIndex/></Suspense>,
+        children: [
+            {
+                path: "list",
+                element: <Suspense fallback={Loading}><MarketList/></Suspense>
+            }
+        ]
     }
 ])
 export default root;
