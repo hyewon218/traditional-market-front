70cd481998a08910fe3c09cbb29a83324a901198
Hyewon Choi <won_11111@naver.com>
2024-06-12 22:14:35 +0900

    [Feature] SSE 실시간 알림 구현 (#15)

---
+++ src/pages/notification/NotificationListPage.js
@@ -0,0 +1,9 @@
+import NotificationLayout from "../../layouts/NotificationLayout";
+
+const ChatRoomListPage = () => {
+
+    return (
+        <NotificationLayout></NotificationLayout>
+    );
+}
+export default ChatRoomListPage;
