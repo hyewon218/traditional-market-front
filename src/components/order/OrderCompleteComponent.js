9456bba1195729dbf1c62104d4261a213d7d656b
hyewon218 <won_11111@naver.com>
2024-07-27 16:45:15 +0900

    [Refactor] 결제 완료 후 해당 주문 정보 조회

---
+++ src/components/order/OrderCompleteComponent.js
@@ -0,0 +1,220 @@
+/**
+ =========================================================
+ * Material Dashboard 2 React - v2.1.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+ Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+
+import * as React from 'react';
+import {useEffect, useMemo, useState} from 'react';
+
+// @mui material components
+import Card from '@mui/material/Card';
+
+// Material Dashboard 2 React components
+import MDBox from '../../components/MD/MDBox';
+
+// Material Dashboard 2 React example components
+import DashboardLayout from '../../examples/LayoutContainers/DashboardLayout';
+import MDTypography from "../MD/MDTypography";
+import {getOrder} from "../../api/orderApi";
+import Grid from "@mui/material/Grid";
+
+const initState = {
+    orderNo: '',
+    orderStatus: '',
+    orderDate: '',
+    deliveryAddr: '',
+    total: '',
+    orderItemList: []
+}
+function OrderCompleteComponent() {
+
+    const [order, setOrder] = useState(initState);
+
+    const total = useMemo(() => {
+        let total = 0
+        for (const orderItem of order.orderItemList) {
+            //console.log("orderItem :", orderItem); // 디버깅 로그 추가
+            total += orderItem.orderPrice * orderItem.count;
+            //console.log("total : "+total);
+        }
+        return total
+    },);
+
+    const handleGetOrder = () => {
+        console.log('handleGetOrderItems');
+        getOrder().then(data => {
+            setOrder(data);
+        }).catch(error => {
+            console.error("주문 상품 목록 조회에 실패했습니다.", error);
+        });
+    };
+
+    useEffect(() => {
+        handleGetOrder();
+    }, []);
+
+    return (
+        <DashboardLayout>
+            <MDBox pt={6} pb={3}>
+                <Card>
+                    <MDBox pt={4} pb={3} px={3}>
+                        <MDBox component="form" role="form">
+                            <MDBox mb={2}>
+                                <MDTypography fontWeight="bold"
+                                              variant="body2">
+                                    주문 번호 : {order.orderNo}
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox mb={2}>
+                                <MDTypography fontWeight="bold"
+                                              variant="body2">
+                                    결제 상태 : {order.orderStatus}
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox mb={2}>
+                                <MDTypography fontWeight="bold"
+                                              variant="body2">
+                                    주문 날짜 : {order.orderDate}
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox mb={2}>
+                                <MDTypography fontWeight="bold"
+                                              variant="body2">
+                                    배송지 정보 : {order.deliveryAddr}
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox mb={2}>
+                                <MDTypography fontWeight="bold"
+                                              variant="body2">
+                                    총 가격 : {total}
+                                </MDTypography>
+                            </MDBox>
+                        </MDBox>
+                    </MDBox>
+                </Card>
+            </MDBox>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <MDTypography
+                        fontWeight="bold"
+                        variant="body2"
+                        fontSize="25px"
+                    >
+                        주문상품
+                    </MDTypography>
+                    <Grid container spacing={4}>
+                        <Grid item xs={7}>
+                            <MDBox pb={3}>
+                                <Card>
+                                    <MDBox pt={2}>
+                                        <div>
+                                            <ul>
+                                                 {/*cartItems 가 배열인 경우에만 map 함수를 실행*/}
+                                                {Array.isArray(
+                                                        order.orderItemList)
+                                                    && order.orderItemList.map(
+                                                        orderItem =>
+                                                            <li key={orderItem.orderItemNo}
+                                                                style={{marginBottom: '16px'}}>
+                                                                <MDBox
+                                                                    pt={2}
+                                                                    px={2}>
+                                                                    <Grid
+                                                                        container
+                                                                        spacing={2}>
+                                                                        <Grid
+                                                                            item
+                                                                            xs={12}>
+                                                                            <MDTypography
+                                                                                fontWeight="bold"
+                                                                                variant="body2">
+
+                                                                            </MDTypography>
+                                                                        </Grid>
+                                                                    </Grid>
+
+                                                                    <Grid
+                                                                        container
+                                                                        spacing={2}>
+                                                                        <Grid
+                                                                            item
+                                                                            xs={7}>
+                                                                            <div
+                                                                                className=" m-1 p-1 ">
+                                                                                {orderItem.imageList.map(
+                                                                                    (imageUrl,
+                                                                                        index) => (
+                                                                                        <img
+                                                                                            key={index}
+                                                                                            src={imageUrl.imageUrl}
+                                                                                            alt={orderItem.itemName}/>
+                                                                                    ))}
+                                                                            </div>
+                                                                        </Grid>
+                                                                        <Grid
+                                                                            item
+                                                                            xs={5}
+                                                                            sx={{mt: 3}}>
+                                                                            <MDTypography
+                                                                                fontWeight="bold"
+                                                                                sx={{fontSize: '2.5rem'}}
+                                                                                variant="body2">
+                                                                                {orderItem.itemName}
+                                                                            </MDTypography>
+                                                                            <MDTypography
+                                                                                fontWeight="bold"
+                                                                                sx={{fontSize: '3rem'}}
+                                                                                variant="body2">
+                                                                                {orderItem.orderPrice
+                                                                                    * orderItem.count}원
+                                                                            </MDTypography>
+                                                                            <Grid
+                                                                                container
+                                                                                sx={{mt: 3}}>
+
+                                                                                <Grid
+                                                                                    item
+                                                                                    xs={3}
+                                                                                    sx={{mt: 1}}>
+                                                                                    <MDTypography
+                                                                                        fontWeight="bold"
+                                                                                        variant="body2"> 수량
+                                                                                        :
+                                                                                        {orderItem.count}개
+                                                                                    </MDTypography>
+                                                                                </Grid>
+                                                                                <Grid
+                                                                                    item
+                                                                                    xs={2}>
+
+                                                                                </Grid>
+                                                                            </Grid>
+                                                                        </Grid>
+                                                                    </Grid>
+                                                                </MDBox>
+                                                            </li>
+                                                    )}
+                                            </ul>
+                                        </div>
+                                    </MDBox>
+                                </Card>
+                            </MDBox>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </DashboardLayout>
+    );
+}
+
+export default OrderCompleteComponent;
--- src/components/route/RouteComponent.js
+++ src/components/route/RouteComponent.js
@@ -9,12 +9,13 @@ import SignUp from '../../layouts/authentication/sign-up';
 import Chat from '../../layouts/chat';
 import Alarm from '../../layouts/alarm';
 import Cart from '../../layouts/cart';
+import Notice from '../../layouts/notice';
 import MemberList from '../../layouts/memberList';
 
 const RoutesComponent = () => {
-    const {loginState, isLogin} = useCustomLogin();
+    const {loginState, isAuthorization} = useCustomLogin();
 
-    const routes = [
+    const publicRoutes = [
         {
             type: 'collapse',
             name: '시장',
@@ -22,75 +23,89 @@ const RoutesComponent = () => {
             route: '/market',
             component: <Market/>,
         },
-        !isLogin ? {
-            type: 'collapse',
-            name: '로그인',
-            key: 'sign-in',
-            route: '/authentication/sign-in',
-            component: <SignIn />,
-        } : {
-            type: 'collapse',
-            name: '로그아웃',
-            key: 'logout',
-            route: '/authentication/logout',
-            component: <SignIn />,
-        },
         {
             type: 'collapse',
-            name: '회원가입',
-            key: 'sign-up',
-            route: '/authentication/sign-up',
-            component: <SignUp/>,
-        },
-        {
-            type: 'collapse',
-            name: '내정보',
-            key: 'myposts',
-            route: '/my-post',
-            component: <MyPosts/>,
-        },
-        {
-            type: 'collapse',
-            name: '채팅',
-            key: 'chat',
-            route: '/chat',
-            component: <Chat/>,
-        },
-        {
-            type: 'collapse',
-            name: '알람',
-            key: 'alarms',
-            route: '/alarms',
-            component: <Alarm/>,
-        },
-        {
-            type: 'collapse',
-            name: '장바구니',
-            key: 'cart',
-            route: '/cart',
-            component: <Cart/>,
+            name: '공지사항',
+            key: 'notice',
+            route: '/notice',
+            component: <Notice/>,
         },
     ];
 
-    if (loginState.role === 'ADMIN') {
-        routes.push({
+    const authRoutes = !isAuthorization
+        ? [
+            {
                 type: 'collapse',
-                name: '시장 추가',
-                key: 'post',
-                route: '/post-market',
-                component: <PostMarket/>,
+                name: '로그인',
+                key: 'sign-in',
+                route: '/authentication/sign-in',
+                component: <SignIn/>,
             },
             {
                 type: 'collapse',
-                name: '회원 목록 조회',
-                key: 'member',
-                route: '/member-list',
-                component: <MemberList/>,
+                name: '회원가입',
+                key: 'sign-up',
+                route: '/authentication/sign-up',
+                component: <SignUp/>,
             },
-        );
-    }
 
-    return routes;
+        ]
+        : [
+            {
+                type: 'collapse',
+                name: '로그아웃',
+                key: 'logout',
+                route: '/authentication/logout',
+                component: <SignIn/>,
+            },
+            {
+                type: 'collapse',
+                name: '내정보',
+                key: 'myposts',
+                route: '/my-post',
+                component: <MyPosts/>,
+            },
+            {
+                type: 'collapse',
+                name: '장바구니',
+                key: 'cart',
+                route: '/cart',
+                component: <Cart/>,
+            },
+            {
+                type: 'collapse',
+                name: '채팅',
+                key: 'chat',
+                route: '/chat',
+                component: <Chat/>,
+            },
+            {
+                type: 'collapse',
+                name: '알람',
+                key: 'alarms',
+                route: '/alarms',
+                component: <Alarm/>,
+            },
+        ];
+
+    const adminRoutes = loginState.role === 'ADMIN' ? [
+        {
+            type: 'collapse',
+            name: '시장 추가',
+            key: 'post',
+            route: '/post-market',
+            component: <PostMarket />,
+        },
+        {
+            type: 'collapse',
+            name: '회원 목록 조회',
+            key: 'member',
+            route: '/member-list',
+            component: <MemberList />,
+        },
+    ] : [];
+
+    return [...publicRoutes, ...authRoutes, ...adminRoutes];
 };
 
 export default RoutesComponent;
