c53ab71645159812de192f15ac3cdceb86320d92
hyewon218 <won_11111@naver.com>
2024-08-04 16:08:53 +0900

    [Refactor] 인증되지 않은 Member 로그인 페이지로 이동

--- src/layouts/cart/index.js
+++ src/layouts/cart/index.js
@@ -1,6 +1,13 @@
 import CartComponent from "../../components/cart/CartComponent";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 function Cart() {
+
+    const {moveToLoginReturn, isAuthorization} = useCustomLogin() // 로그인이 필요한 페이지
+
+    if(!isAuthorization){
+        return moveToLoginReturn()
+    }
 
     return (
         <CartComponent/>
