7cd7e9344daec4b74b02c1085f5fdcacde302639
Hyewon Choi <won_11111@naver.com>
2024-08-27 23:37:33 +0900

    [Refactor] 알람 읽음 상태 변경 및 조회 API 연결 (#110)

--- src/api/notificationApi.js
+++ src/api/notificationApi.js
@@ -9,8 +9,20 @@ export const getNotificationList = async () => { // 알람 목록 조회
     return res.data
 }
 
-export const getNotificationCount = async () => { // 알람 수 조회
+export const getNotificationCount = async () => { // 읽지 않은 알람 수 조회
     const res = await axios.get(`${prefixApi}/count`, {
     })
+    return res.data
+}
+
+/*알람 읽음 상태로 변경*/
+export const putIsRead = async (nno) => {
+    const res = await axios.put(`${prefixApi}/${nno}/read`)
+    return res.data
+}
+
+/*알람 읽음 여부 조회*/
+export const getIsRead = async (nno) => {
+    const res = await axios.get(`${prefixApi}/${nno}/read`)
     return res.data
 }
