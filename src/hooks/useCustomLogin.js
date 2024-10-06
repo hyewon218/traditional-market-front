a13d646123f8fd01387efdda58c2216902311d4b
Woncheol SONG <133622405+songwc3@users.noreply.github.com>
2024-10-06 23:49:42 +0900

    [Refactor] 보안 강화를 위해 JWT 토큰을 서버 API를 통해 가져오는 방식으로 변경 (#164)

--- src/hooks/useCustomLogin.js
+++ src/hooks/useCustomLogin.js
@@ -1,21 +1,114 @@
-import {useDispatch, useSelector} from "react-redux"
-import {Navigate, useNavigate} from "react-router-dom"
-import {loginPostAsync, logoutPostAsync} from "../slices/loginSlice"
-import {getCookie} from "../util/cookieUtil";
-import {jwtDecode} from "jwt-decode";
+//import {useDispatch, useSelector} from "react-redux"
+//import {Navigate, useNavigate} from "react-router-dom"
+//import {loginPostAsync, logoutPostAsync} from "../slices/loginSlice"
+//import {getCookie} from "../util/cookieUtil";
+//import {jwtDecode} from "jwt-decode";
+//
+//const useCustomLogin = () => {
+//
+//    const navigate = useNavigate()
+//
+//    const dispatch = useDispatch()
+//
+//    const loginState = useSelector(state => state.loginSlice) //-------로그인 상태
+//
+//    //console.log("현재 로그인 상태: ", loginState)
+//    //const isLogin = loginState.memberId !== "" //----------로그인 여부
+//
+//    const isAuthorization = getCookie('Authorization')
+//
+//    let isAdmin = false;
+//    let isSeller = false;
+//    let isMember = false;
+//    let userId = null;
+//
+//    if (isAuthorization) {
+//        try {
+//            /*JWT 를 디코딩하고 해당 페이로드를 검사*/
+//            const decodedToken = jwtDecode(isAuthorization);
+//            isAdmin = decodedToken.role === 'ROLE_ADMIN';
+//            isSeller = decodedToken.role === 'ROLE_SELLER';
+//            isMember = decodedToken.role === 'ROLE_MEMBER';
+//            userId = decodedToken.sub || decodedToken.id;
+//            //console.log("?!?!??!?"+ isMember)
+//        } catch (error) {
+//            console.error("Invalid token:", error);
+//        }
+//    }
+//
+//    const doLogin = async (loginParam) => { //----------로그인 함수
+//        const action = await dispatch(loginPostAsync(loginParam))
+//        //console.log("doLogin 액션 페이로드: ", action.payload)
+//        return action.payload
+//    }
+//
+//    const doLogout = async () => { //---------------로그아웃 함수
+//        await dispatch(logoutPostAsync())
+//    }
+//
+//    const moveToPath = (path) => {  //----------------페이지 이동
+//        navigate({pathname: path}, {replace: true})
+//    }
+//
+//    const moveToLogin = () => { //----------------------로그인 페이지로 이동
+//        navigate({pathname: '/authentication/sign-in'}, {replace: true})
+//    }
+//
+//    const moveToLoginReturn = () => { //----------------------로그인 페이지로 이동 컴포넌트
+//        return <Navigate replace to="/authentication/sign-in"/>
+//    }
+//
+//    return {
+//        loginState,
+//        //isLogin,
+//        doLogin,
+//        doLogout,
+//        moveToPath,
+//        moveToLogin,
+//        moveToLoginReturn,
+//        isAuthorization,
+//        isAdmin,
+//        isSeller,
+//        isMember,
+//        userId
+//    }
+//
+//}
+//
+//export default useCustomLogin
+
+
+
+
+
+// 서버에서 액세스토큰 호출 함수를 통해 isAuthorization 정의
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { loginPostAsync, logoutPostAsync, setAuthorization, clearAuthorization } from "../slices/loginSlice";
+import { getCookie } from "../util/cookieUtil";
+import { jwtDecode } from "jwt-decode";
+import { getAccessToken } from "../api/tokenApi";
+import { useEffect } from "react";
 
 const useCustomLogin = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    const navigate = useNavigate()
+    const loginState = useSelector(state => state.loginSlice);
+    const { isAuthorization } = loginState;
 
-    const dispatch = useDispatch()
+    useEffect(() => {
+        const fetchAccessToken = async () => {
+            try {
+                const token = await getAccessToken();
+                dispatch(setAuthorization(token)); // 액세스 토큰을 Redux 상태에 설정
+            } catch (error) {
+                console.error("액세스 토큰 가져오기 오류:", error);
+            }
+        };
 
-    const loginState = useSelector(state => state.loginSlice) //-------로그인 상태
-
-    //console.log("현재 로그인 상태: ", loginState)
-    //const isLogin = loginState.memberId !== "" //----------로그인 여부
-
-    const isAuthorization = getCookie('Authorization')
+        fetchAccessToken();
+    }, [dispatch]);
 
     let isAdmin = false;
     let isSeller = false;
@@ -24,43 +117,40 @@ const useCustomLogin = () => {
 
     if (isAuthorization) {
         try {
-            /*JWT 를 디코딩하고 해당 페이로드를 검사*/
             const decodedToken = jwtDecode(isAuthorization);
             isAdmin = decodedToken.role === 'ROLE_ADMIN';
             isSeller = decodedToken.role === 'ROLE_SELLER';
             isMember = decodedToken.role === 'ROLE_MEMBER';
             userId = decodedToken.sub || decodedToken.id;
-            //console.log("?!?!??!?"+ isMember)
         } catch (error) {
             console.error("Invalid token:", error);
         }
     }
 
-    const doLogin = async (loginParam) => { //----------로그인 함수
-        const action = await dispatch(loginPostAsync(loginParam))
-        //console.log("doLogin 액션 페이로드: ", action.payload)
-        return action.payload
+    const doLogin = async (loginParam) => {
+        const action = await dispatch(loginPostAsync(loginParam));
+        return action.payload;
     }
 
-    const doLogout = async () => { //---------------로그아웃 함수
-        await dispatch(logoutPostAsync())
+    const doLogout = async () => {
+        await dispatch(logoutPostAsync());
+        dispatch(clearAuthorization()); // 로그아웃 시 Redux 상태 초기화
     }
 
-    const moveToPath = (path) => {  //----------------페이지 이동
-        navigate({pathname: path}, {replace: true})
+    const moveToPath = (path) => {
+        navigate({ pathname: path }, { replace: true });
     }
 
-    const moveToLogin = () => { //----------------------로그인 페이지로 이동
-        navigate({pathname: '/authentication/sign-in'}, {replace: true})
+    const moveToLogin = () => {
+        navigate({ pathname: '/authentication/sign-in' }, { replace: true });
     }
 
-    const moveToLoginReturn = () => { //----------------------로그인 페이지로 이동 컴포넌트
-        return <Navigate replace to="/authentication/sign-in"/>
+    const moveToLoginReturn = () => {
+        return <Navigate replace to="/authentication/sign-in" />;
     }
 
     return {
         loginState,
-        //isLogin,
         doLogin,
         doLogout,
         moveToPath,
@@ -72,7 +162,6 @@ const useCustomLogin = () => {
         isMember,
         userId
     }
-
 }
 
-export default useCustomLogin
+export default useCustomLogin;
--- src/slices/loginSlice.js
+++ src/slices/loginSlice.js
@@ -1,18 +1,92 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {loginPost, logoutPost} from "../api/memberApi";
+//import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+//import {loginPost, logoutPost} from "../api/memberApi";
+//
+//// 초기 상태는 로그인되지 않은 상태로 가정
+//const initState = {
+//    memberId: ''
+//}
+//
+//export const loginPostAsync = createAsyncThunk('loginPostAsync', (param) => {
+//    return loginPost(param)
+//})
+//
+//export const logoutPostAsync = createAsyncThunk('logoutAsync',  () => {
+//    return logoutPost();
+//})
+//
+//const loginSlice = createSlice({
+//    name: 'loginSlice',
+//    initialState: initState,
+//
+//    reducers: {
+//        login: (state, action) => {
+//            console.log("login.....")
+//            //{memberId, memberPw로 구성}
+//            const data = action.payload
+//            //새로운 상태
+//            return {memberId: data.memberId}
+//        },
+//        logout: () => {
+//            console.log("logout....")
+//            return {...initState}
+//        }
+//    },
+//
+//    extraReducers: (builder) => {
+//        builder.addCase(loginPostAsync.fulfilled, (state, action) => {
+//            console.log("fulfilled")
+//            return action.payload;
+//        })
+//        .addCase(loginPostAsync.pending, () => {
+//            console.log("pending")
+//        })
+//        .addCase(loginPostAsync.rejected, () => {
+//            console.log("rejected")
+//        })
+//        .addCase(logoutPostAsync.fulfilled, () => {
+//            console.log("logout fulfilled")
+//            return { ...initState };
+//        })
+//        .addCase(logoutPostAsync.pending, () => {
+//            console.log("logout pending")
+//        })
+//        .addCase(logoutPostAsync.rejected, () => {
+//            console.log("logout rejected")
+//        })
+//    }
+//})
+//
+//export const {login, logout} = loginSlice.actions
+//
+//export default loginSlice.reducer
 
-// 초기 상태는 로그인되지 않은 상태로 가정
+
+
+// 액세스토큰 전역적으로 관리하는 코드 추가
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { loginPost, logoutPost } from "../api/memberApi";
+import { getAccessToken } from '../api/tokenApi';
+
+// 초기 상태 설정
 const initState = {
-    memberId: ''
+    memberId: '',
+    isAuthorization: null, // 추가된 상태
 }
 
-export const loginPostAsync = createAsyncThunk('loginPostAsync', (param) => {
-    return loginPost(param)
-})
+// 로그인 비동기 액션
+export const loginPostAsync = createAsyncThunk('loginPostAsync', async (param) => {
+    return loginPost(param);
+});
 
-export const logoutPostAsync = createAsyncThunk('logoutAsync',  () => {
+// 로그아웃 비동기 액션
+export const logoutPostAsync = createAsyncThunk('logoutAsync', async () => {
     return logoutPost();
-})
+});
+
+// 액세스 토큰 가져오는 비동기 액션
+export const fetchAccessTokenAsync = createAsyncThunk('fetchAccessTokenAsync', async () => {
+    return getAccessToken();
+});
 
 const loginSlice = createSlice({
     name: 'loginSlice',
@@ -20,42 +94,58 @@ const loginSlice = createSlice({
 
     reducers: {
         login: (state, action) => {
-            console.log("login.....")
-            //{memberId, memberPw로 구성}
-            const data = action.payload
-            //새로운 상태
-            return {memberId: data.memberId}
+            console.log("login.....");
+            const data = action.payload;
+            state.memberId = data.memberId;
         },
-        logout: () => {
-            console.log("logout....")
-            return {...initState}
+        logout: (state) => {
+            console.log("logout....");
+            return { ...initState };
+        },
+        setAuthorization: (state, action) => {
+            state.isAuthorization = action.payload;
+        },
+        clearAuthorization: (state) => {
+            state.isAuthorization = null;
         }
     },
 
     extraReducers: (builder) => {
+        // 로그인 비동기 처리
         builder.addCase(loginPostAsync.fulfilled, (state, action) => {
-            console.log("fulfilled")
-            return action.payload;
+            console.log("login fulfilled");
+            state.memberId = action.payload.memberId; // 로그인 시 memberId 업데이트
         })
         .addCase(loginPostAsync.pending, () => {
-            console.log("pending")
+            console.log("login pending");
         })
         .addCase(loginPostAsync.rejected, () => {
-            console.log("rejected")
+            console.log("login rejected");
         })
-        .addCase(logoutPostAsync.fulfilled, () => {
-            console.log("logout fulfilled")
+
+        // 로그아웃 비동기 처리
+        .addCase(logoutPostAsync.fulfilled, (state) => {
+            console.log("logout fulfilled");
             return { ...initState };
         })
         .addCase(logoutPostAsync.pending, () => {
-            console.log("logout pending")
+            console.log("logout pending");
         })
         .addCase(logoutPostAsync.rejected, () => {
-            console.log("logout rejected")
+            console.log("logout rejected");
         })
+
+        // 액세스 토큰 가져오기 비동기 처리
+        .addCase(fetchAccessTokenAsync.fulfilled, (state, action) => {
+            console.log("access token fulfilled");
+            state.isAuthorization = action.payload; // 액세스 토큰 저장
+        })
+        .addCase(fetchAccessTokenAsync.rejected, () => {
+            console.log("access token rejected");
+        });
     }
-})
+});
 
-export const {login, logout} = loginSlice.actions
+export const { login, logout, setAuthorization, clearAuthorization } = loginSlice.actions;
 
-export default loginSlice.reducer
+export default loginSlice.reducer;
